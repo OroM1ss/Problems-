b3c9c7e2441e5051976fc67f93140f5eb1766913
Oleg Fukanchik <oleg.fukanchik.01@gmial.com>
2026-03-07 22:03:52 +0300

    added problem b

--- Problem 77.js
+++ Problem 77.js
@@ -10,5 +10,19 @@ let pow_2 = function (n) {
 for (let i =0;i<=10;i++){
     console.log(i,pow_2(i))
 }
+!8 = 1*2*3*4*5*6*7*8
+
+let fuctorial = function(n){    
+    let product = 1;
+    for (let i = 1; i<=n ; i++){
+        product = product*i;
+    }
+    return product 
+        
+        
+}
+
+console.log (fuctorial (3));
+console.log (fuctorial (3));
 
 
