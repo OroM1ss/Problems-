72ee52529595f22cb503fb05afe0e8f5d377e876
Oleg Fukanchik <oleg.fukanchik.01@gmial.com>
2026-03-08 17:55:20 +0300

    problem 77 punct v solved

--- Problem 77.js
+++ Problem 77.js
@@ -1,28 +1,43 @@
-let pow_2 = function (n) {
-    let porduct = 1;
-    for (let i = 0; i < n; i++) {
-        porduct = porduct * 2;
+// let pow_2 = function (n) {
+//     let porduct = 1;
+//     for (let i = 0; i < n; i++) {
+//         porduct = porduct * 2;
 
+//     }
+//     return porduct
+// }
+
+// for (let i =0;i<=10;i++){
+//     console.log(i,pow_2(i))
+// }
+// !8 = 1*2*3*4*5*6*7*8
+
+// let fuctorial = function(n){    
+//     let product = 1;
+//     for (let i = 1; i<=n ; i++){
+//         product = product*i;
+//     }
+//     return product 
+
+
+// }
+
+// console.log (fuctorial (3));
+// console.log (fuctorial (3));
+
+
+
+
+let problem_v = function (n) {
+    let expression = 1;
+    let main_product = 1;
+    for (let i = 1; i <= n; i++) {
+        expression = (1 + (1 / (i * i)));
+        main_product = main_product * expression
     }
-    return porduct
+    return main_product
+
+
 }
 
-for (let i =0;i<=10;i++){
-    console.log(i,pow_2(i))
-}
-!8 = 1*2*3*4*5*6*7*8
-
-let fuctorial = function(n){    
-    let product = 1;
-    for (let i = 1; i<=n ; i++){
-        product = product*i;
-    }
-    return product 
-        
-        
-}
-
-console.log (fuctorial (3));
-console.log (fuctorial (3));
-
-
+console.log(problem_v(3));
